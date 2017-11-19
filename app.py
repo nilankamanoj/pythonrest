ea8c1b8f6e0b5ac82ccb8ecd531aa19d5f6c8ba6
nilankamanoj <nilankaeng16@gmail.com>
2017-11-19 22:06:50 +0530

    added source files

---
+++ app.py
@@ -0,0 +1,53 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+@app.route('/todo/api/v1.0/sum/<int:int1>/<int:int2>', methods=['GET'])
+def get_sum(int1,int2):
+    return jsonify({'sum': int1+int2})
+
+if __name__ == '__main__':
+    app.run(debug=True)
